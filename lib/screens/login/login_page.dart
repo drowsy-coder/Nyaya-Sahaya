5e76b3c66ec591139a1946b8d635a8d43d97e83b
Amritansh Sharma <amritansh.code@gmail.com>
2023-09-17 11:39:10 +0530

    lawyer login

--- lib/screens/login/login_page.dart
+++ lib/screens/login/login_page.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:nyaya_sahaya/screens/stakeholders/lawyer_home.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../stakeholders/client_home.dart';
+
+enum UserRole {
+  lawyer,
+  client,
+}
+
+String userRoleToString(UserRole role) {
+  return role.toString().split('.').last;
+}
+
+UserRole stringToUserRole(String role) {
+  return UserRole.values.firstWhere(
+    (e) => e.toString().split('.').last == role,
+    orElse: () => UserRole.client,
+  );
+}
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -15,13 +32,14 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _caseNumberController = TextEditingController();
+  final TextEditingController _identifierController = TextEditingController();
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   bool _isLoginForm = true;
   bool _isLoading = false;
+  UserRole _userRole = UserRole.client; // Default to client role
 
   late SharedPreferences _prefs;
 
@@ -31,111 +49,160 @@ class _LoginPageState extends State<LoginPage> {
     _initSharedPreferences();
   }
 
-   void _initSharedPreferences() async {
-  _prefs = await SharedPreferences.getInstance();
+  void _initSharedPreferences() async {
+    _prefs = await SharedPreferences.getInstance();
 
-  final String? uid = _prefs.getString('uid');
-  if (uid != null) {
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const ClientHomePage()));
+    final String? uid = _prefs.getString('uid');
+    if (uid != null) {
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => const ClientHomePage()));
+    }
   }
-}
 
-
-  void _saveUserDataToPrefs(String uid, String email, String caseNumber) {
+  void _saveUserDataToPrefs(String uid, String email, String identifier) {
     _prefs.setString('uid', uid);
     _prefs.setString('email', email);
-    _prefs.setString('caseNumber', caseNumber);
+    _prefs.setString('identifier', identifier);
   }
 
-  void _logout() {
-  _prefs.remove('uid');
-  _prefs.remove('email');
-  _prefs.remove('caseNumber');
-  _prefs.remove('isLoggedIn');
-
-  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const LoginPage()));
-}
-
-
-  Future<void> _signInWithEmail(String email, String password) async {
-  setState(() {
-    _isLoading = true;
-  });
-
-  try {
-    final UserCredential userCredential = await _auth.signInWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-    final User user = userCredential.user!;
-
-    await _storeUserData(user.uid);
-
-    _saveUserDataToPrefs(user.uid, email, _caseNumberController.text.trim());
-
-    _prefs.setBool('isLoggedIn', true);
-
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const ClientHomePage()));
-  } on FirebaseAuthException catch (e) {
+  Future<void> _signInWithEmail(
+      String email, String password, String identifier) async {
     setState(() {
-      _isLoading = false;
+      _isLoading = true;
     });
 
-    print('Failed to sign in with email and password: ${e.message}');
+    try {
+      final UserCredential userCredential =
+          await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      final User user = userCredential.user!;
+      final String uid = user.uid;
+
+      final DocumentSnapshot userData =
+          await _firestore.collection('users').doc(uid).get();
+
+      if (userData.exists) {
+        final String storedUserRole = userData['userRole'];
+        final UserRole userRole = stringToUserRole(storedUserRole);
+
+        if (userRole == _userRole && userData['identifier'] == identifier) {
+          await _storeUserData(uid, email, identifier);
+
+          _saveUserDataToPrefs(uid, email, identifier);
+
+          _prefs.setBool('isLoggedIn', true);
+
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text('Login Failed'),
+                content: const Text('Invalid credentials or user type.'),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('OK'),
+                  ),
+                ],
+              );
+            },
+          );
+        }
+      } else {
+        setState(() {
+          _isLoading = false;
+        });
+
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text('Login Failed'),
+              content: const Text('User data not found.'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+
+      print('Failed to sign in with email and password: ${e.message}');
+    }
   }
-}
 
-Future<void> _createAccount(String email, String password) async {
-  setState(() {
-    _isLoading = true;
-  });
-
-  try {
-    final UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-    final User user = userCredential.user!;
-
-    await _storeUserData(user.uid);
-
-    _saveUserDataToPrefs(user.uid, email, _caseNumberController.text.trim());
-
-    _prefs.setBool('isLoggedIn', true);
-
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const ClientHomePage()));
-  } on FirebaseAuthException catch (e) {
+  Future<void> _createAccount(
+      String email, String password, String identifier) async {
     setState(() {
-      _isLoading = false;
+      _isLoading = true;
     });
 
-    print('Failed to create user account: ${e.message}');
+    try {
+      final UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      final User user = userCredential.user!;
+      final String uid = user.uid;
+
+      await _storeUserData(uid, email, identifier);
+
+      _saveUserDataToPrefs(uid, email, identifier);
+
+      _prefs.setBool('isLoggedIn', true);
+
+      if (_userRole == UserRole.client) {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const ClientHomePage()));
+      } else if (_userRole == UserRole.lawyer) {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const LawyerHomePage()));
+      }
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+
+      print('Failed to create user account: ${e.message}');
+    }
   }
-}
 
-  Future<void> _storeUserData(String uid) async {
-    final String email = _emailController.text.trim();
-    final String caseNumber = _caseNumberController.text.trim();
-
+  Future<void> _storeUserData(
+      String uid, String email, String identifier) async {
     await _firestore.collection('users').doc(uid).set({
       'name': email.split('@')[0],
       'email': email,
-      'caseNumber': caseNumber,
+      'identifier': identifier,
+      'userRole': userRoleToString(_userRole),
+      if (_userRole == UserRole.client) 'caseNumber': identifier,
+      if (_userRole == UserRole.lawyer) 'barNumber': identifier,
       'uid': uid,
     });
   }
 
   void _submitForm() {
+    final String email = _emailController.text.trim();
+    final String password = _passwordController.text.trim();
+    final String identifier = _identifierController.text.trim();
+
     if (_isLoginForm) {
-      final String email = _emailController.text.trim();
-      final String password = _passwordController.text.trim();
-
-      _signInWithEmail(email, password);
+      _signInWithEmail(email, password, identifier);
     } else {
-      final String email = _emailController.text.trim();
-      final String password = _passwordController.text.trim();
-
-      _createAccount(email, password);
+      _createAccount(email, password, identifier);
     }
   }
 
@@ -151,6 +218,25 @@ Future<void> _createAccount(String email, String password) async {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
+              // Role selection
+              DropdownButtonFormField<UserRole>(
+                value: _userRole,
+                onChanged: (UserRole? newValue) {
+                  setState(() {
+                    _userRole = newValue!;
+                  });
+                },
+                items: [
+                  DropdownMenuItem(
+                    value: UserRole.client,
+                    child: Text('Client'),
+                  ),
+                  DropdownMenuItem(
+                    value: UserRole.lawyer,
+                    child: Text('Lawyer'),
+                  ),
+                ],
+              ),
               TextFormField(
                 controller: _emailController,
                 decoration: const InputDecoration(labelText: 'Email'),
@@ -160,9 +246,14 @@ Future<void> _createAccount(String email, String password) async {
                 decoration: const InputDecoration(labelText: 'Password'),
                 obscureText: true,
               ),
-              if (!_isLoginForm)
+              if (_userRole == UserRole.lawyer)
                 TextFormField(
-                  controller: _caseNumberController,
+                  controller: _identifierController,
+                  decoration: const InputDecoration(labelText: 'Bar Number'),
+                ),
+              if (_userRole == UserRole.client)
+                TextFormField(
+                  controller: _identifierController,
                   decoration: const InputDecoration(labelText: 'Case Number'),
                 ),
               const SizedBox(height: 16.0),
@@ -172,10 +263,11 @@ Future<void> _createAccount(String email, String password) async {
               ),
               TextButton(
                 onPressed: _toggleFormMode,
-                child: Text(_isLoginForm ? 'Create an account' : 'Have an account? Sign in'),
+                child: Text(_isLoginForm
+                    ? 'Create an account'
+                    : 'Have an account? Sign in'),
               ),
-              if (_isLoading)
-                const CircularProgressIndicator(),
+              if (_isLoading) const CircularProgressIndicator(),
             ],
           ),
         ),
--- lib/screens/stakeholders/lawyer_home.dart
+++ lib/screens/stakeholders/lawyer_home.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
+import '../../widgets/buttons/logout_button.dart';
+
 class LawyerHomePage extends StatelessWidget {
   const LawyerHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Lawyer Home Page');
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Lawyer Home Page')),
+        body: LogoutButton(),
+      ),
+    );
   }
 }
