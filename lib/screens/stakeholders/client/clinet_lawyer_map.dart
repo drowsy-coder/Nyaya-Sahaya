e2cf9b95413d9bc07a071ef2a72bfe5b64038866
Amritansh Sharma <amritansh.code@gmail.com>
2023-09-21 09:58:36 +0530

    .

--- lib/screens/stakeholders/client/clinet_lawyer_map.dart
+++ lib/screens/stakeholders/client/clinet_lawyer_map.dart
@@ -13,18 +13,18 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   final Completer<GoogleMapController> _controller = Completer();
   static const CameraPosition _initialCameraPosition = CameraPosition(
-    target: LatLng(28.6139, 77.2090), // New Delhi's coordinates
+    target: LatLng(28.6139, 77.2090),
     zoom: 11,
   );
 
   final List<Marker> _markers = [
     const Marker(
       markerId: MarkerId('1'),
-      position: LatLng(28.6139, 77.2090), // New Delhi's coordinates
+      position: LatLng(28.6139, 77.2090),
     ),
     const Marker(
       markerId: MarkerId('2'),
-      position: LatLng(28.6239, 77.2090), // New Delhi's coordinates
+      position: LatLng(28.6239, 77.2090),
     ),
   ];
 
