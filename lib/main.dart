0d3e35a2f4c31d4e6704321b9dc6848c4253546e
Amritansh Sharma <amritansh.code@gmail.com>
2023-09-22 10:40:19 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Firebase Login',
       theme: ThemeData.dark(),
-      home: LawyerScreen(),
+      home: ClientScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
