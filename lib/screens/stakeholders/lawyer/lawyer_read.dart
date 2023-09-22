d7f7845b3bfe561d930bf5721818b12da5909356
amspsingh04 <sp.singh.150604@gmail.com>
2023-09-22 22:28:35 +0530

    cards added

--- lib/screens/stakeholders/lawyer/lawyer_read.dart
+++ lib/screens/stakeholders/lawyer/lawyer_read.dart
@@ -6,8 +6,31 @@ class LawyerReadScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: LogoutButton(),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Card Example'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(10), // Adjust padding as needed
+        children: <Widget>[
+          Card(
+            elevation: 3, // Adjust elevation for shadow effect
+            margin: const EdgeInsets.all(10), // Adjust margin as needed
+            child: ListTile(
+              leading: const Icon(
+                  Icons.account_circle), // Optional: Add an icon or image
+              title: const Text('Card Title'), // Add a title
+              subtitle: const Text('Card Subtitle'), // Add a subtitle
+              trailing: IconButton(
+                icon: const Icon(Icons.more_vert),
+                onPressed: () {
+                  // Add action on button press if needed
+                },
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
