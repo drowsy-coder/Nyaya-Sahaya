e42c027b014f0f7d53abff5d375eca67b8db8607
Yash Rathi <yashrathicricket@gmail.com>
2023-09-23 05:51:46 +0530

    Update vocational ed

--- lib/screens/stakeholders/client/support/vocational_support/screens/computer_training.dart
+++ lib/screens/stakeholders/client/support/vocational_support/screens/computer_training.dart
@@ -1,127 +1,159 @@
 import 'package:flutter/material.dart';
+import 'package:law/screens/stakeholders/client/support/mental/m_player.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: ComputerTrainingScreen(),
+  ));
+}
 
 class ComputerTrainingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Computer Training'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Computer Training',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-        body: MyGridView(),
+        backgroundColor: Colors.grey[900],
+      ),
+      body: ListView(
+        children: const [
+          VideoCard(
+            videoId: 'y2kg3MOk1sY',
+            title: 'Computer Technology Basics',
+            thumbnailUrl: 'assets/images/ct1.jpg',
+            description: 'freeCodeCamp',
+          ),
+          VideoCard(
+            videoId: 'S-nHYzK-BVg',
+            title: 'Microsoft Word for beginners',
+            thumbnailUrl: 'assets/images/ct2.jpg',
+            description: 'Technology for Teachers and Students',
+          ),
+          VideoCard(
+            videoId: 'LgXzzu68j7M',
+            title: 'Excel Tutorial in 15 min',
+            thumbnailUrl: 'assets/images/voc4.jpg',
+            description: 'Kevin Stewart',
+          ),
+          VideoCard(
+            videoId: 'LxgheItBIzQ',
+            title: 'Top 15 Microsoft Word Tips & Tricks',
+            thumbnailUrl: 'assets/images/ct4.jpg',
+            description: 'Kevin Stewart',
+          ),
+        ],
       ),
     );
   }
 }
 
-class MyGridView extends StatelessWidget {
-  final List<CardItem> cardItems = [
-    CardItem(
-      image: 'assets/images/cn.jpg',
-      header: 'Computer Networks',
-      externalLink: 'https://archive.nptel.ac.in/courses/106/105/106105183/',
-    ),
-    CardItem(
-      image: 'assets/images/cpp.jpg',
-      header: 'Programming in C++',
-      externalLink: 'https://onlinecourses.nptel.ac.in/noc21_cs02/preview',
-    ),
-    CardItem(
-      image: 'assets/images/cao.jpg',
-      header: 'Computer Architecture',
-      externalLink: 'https://nptel.ac.in/courses/106102062',
-    ),
-    CardItem(
-      image: 'assets/images/computer_graphics.jpg',
-      header: 'Computer Graphics',
-      externalLink: 'https://nptel.ac.in/courses/106102062',
-    ),
-    CardItem(
-      image: 'assets/images/java_programming.jpg',
-      header: 'Java Programming',
-      externalLink: 'https://nptel.ac.in/courses/106102062',
-    ),
-    CardItem(
-      image: 'assets/images/dsa.jpg',
-      header: 'Data Structures and Algorithms',
-      externalLink: 'https://nptel.ac.in/courses/106102062',
-    ),
-  ];
+class VideoCard extends StatelessWidget {
+  final String videoId;
+  final String title;
+  final String thumbnailUrl;
+  final String description;
 
-  @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2, // Number of columns
-      ),
-      itemCount: cardItems.length,
-      itemBuilder: (context, index) {
-        return MyCard(
-          image: cardItems[index].image,
-          header: cardItems[index].header,
-          onTap: () {
-            launch(cardItems[index].externalLink); // Open external link
-          },
-        );
-      },
-    );
-  }
-}
-
-class CardItem {
-  final String image;
-  final String header;
-  final String externalLink;
-
-  CardItem({
-    required this.image,
-    required this.header,
-    required this.externalLink,
-  });
-}
-
-class MyCard extends StatelessWidget {
-  final String image;
-  final String header;
-  final VoidCallback? onTap;
-
-  MyCard({
-    required this.image,
-    required this.header,
-    this.onTap,
+  const VideoCard({
+    required this.videoId,
+    required this.title,
+    required this.thumbnailUrl,
+    required this.description,
   });
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 5,
+      margin: const EdgeInsets.all(16.0),
+      elevation: 12, // Increased elevation for a stronger card effect
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
+        borderRadius:
+            BorderRadius.circular(16.0), // Rounded corners for the card
       ),
       child: InkWell(
-        onTap: onTap, // Invoke the onTap callback when clicked
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Image.asset(
-              image,
-              height: 130,
-              width: double.infinity,
-              fit: BoxFit
-                  .cover, // Ensure the image fits within the available space
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                header,
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
+        onTap: () {
+          // Navigate to the player screen with the selected video
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => VideoPlayerScreen(
+                videoId: videoId,
+                title: title,
+                description: description,
               ),
             ),
-          ],
+          );
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(
+                16.0), // Rounded corners for the container
+            gradient: LinearGradient(
+              colors: [
+                Colors.grey[700]!.withOpacity(0.8),
+                Colors.grey[850]!.withOpacity(0.9)
+              ], // Grey gradient
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.8),
+                spreadRadius: 4,
+                blurRadius: 10,
+                offset: const Offset(0, 5),
+              ),
+            ],
+          ),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16.0),
+                  child: Image.asset(
+                    thumbnailUrl,
+                    width: 140,
+                    height: 120,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 7,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        title,
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.yellow[700],
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        description,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
