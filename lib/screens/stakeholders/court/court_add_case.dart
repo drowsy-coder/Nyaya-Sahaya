3ac1b9ba5dcefb9706e572ca546bada19c441c1f
Amritansh Sharma <amritansh.code@gmail.com>
2024-02-19 00:30:56 +0530

    case add method ui imprv

--- lib/screens/stakeholders/court/court_add_case.dart
+++ lib/screens/stakeholders/court/court_add_case.dart
@@ -7,7 +7,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../../login/login_method.dart';
 
 class CourtAddCase extends StatefulWidget {
-  const CourtAddCase({super.key});
+  const CourtAddCase({Key? key}) : super(key: key);
 
   @override
   _CourtAddCaseState createState() => _CourtAddCaseState();
@@ -24,16 +24,8 @@ class _CourtAddCaseState extends State<CourtAddCase> {
       TextEditingController();
   DateTime? _selectedDate;
 
-  bool _clientEmailFocused = false;
-  bool _lawyerEmailFocused = false;
-  bool _clientNameFocused = false;
-  bool _ipcSectionsFocused = false;
-  bool _nextHearingDateFocused = false;
-
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
   void _logout(BuildContext context) async {
-    await _auth.signOut();
+    await FirebaseAuth.instance.signOut();
 
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.remove('uid');
@@ -46,13 +38,13 @@ class _CourtAddCaseState extends State<CourtAddCase> {
   }
 
   Future<void> _selectDate(BuildContext context) async {
-    final DateTime picked = (await showDatePicker(
+    final DateTime? picked = await showDatePicker(
       context: context,
       initialDate: _selectedDate ?? DateTime.now(),
-      firstDate: DateTime.now(),
+      firstDate: DateTime(2000),
       lastDate: DateTime(2101),
-    ))!;
-    if (picked != _selectedDate) {
+    );
+    if (picked != null && picked != _selectedDate) {
       setState(() {
         _selectedDate = picked;
         _nextHearingDateController.text = "${picked.toLocal()}".split(' ')[0];
@@ -129,17 +121,9 @@ class _CourtAddCaseState extends State<CourtAddCase> {
           print('Error: $error');
         }
       }
+      // Implementation for submitting the case
+      // This is where you will handle the form submission logic
     }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
   }
 
   @override
@@ -147,266 +131,138 @@ class _CourtAddCaseState extends State<CourtAddCase> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add Case'),
+        backgroundColor: Colors.deepPurple, // Improved app bar color
         actions: [
           IconButton(
-              onPressed: () {
-                _logout(context);
-              },
-              icon: const Icon(Icons.logout))
+              onPressed: () => _logout(context), icon: const Icon(Icons.logout))
         ],
       ),
-      body: Center(
+      body: SingleChildScrollView(
+        // Allows the form to be scrollable
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  Image.asset('assets/images/2689736-2.png',
-                      height: 140, width: 140),
-                ],
-              ),
-            ),
-            Container(
-              width: 380,
-              padding: const EdgeInsets.all(16.0),
-              decoration: BoxDecoration(
+            Image.asset('assets/images/2689736-2.png', height: 140, width: 140),
+            const SizedBox(height: 20),
+            Card(
+              // Using Card for a cleaner look
+              elevation: 5,
+              shadowColor: Colors.deepPurpleAccent,
+              shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(20),
-                color: Colors.grey[900],
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey[800]!,
-                    spreadRadius: 2,
-                    blurRadius: 10,
-                    offset: const Offset(0, 4),
-                  ),
-                ],
               ),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TextFormField(
-                      controller: _clientEmailController,
-                      decoration: InputDecoration(
-                          labelText: 'Undertrial Email',
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              width: _clientEmailFocused ? 3 : 1,
-                              color: Colors.lightBlueAccent,
-                            ),
-                            borderRadius: BorderRadius.circular(20.0),
-                          ),
-                          filled: true,
-                          fillColor: Colors.grey[900],
-                          icon: const Icon(
-                            Icons.email,
-                            color: Colors.lightBlueAccent,
-                          )),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter the client\'s email';
-                        }
-                        return null;
-                      },
-                      onTap: () {
-                        setState(() {
-                          _clientEmailFocused = true;
-                          _clientNameFocused = false;
-                          _ipcSectionsFocused = false;
-                          _nextHearingDateFocused = false;
-                        });
-                      },
-                    ),
-                    const SizedBox(height: 10),
-                    TextFormField(
-                      controller: _caseNumberController,
-                      decoration: InputDecoration(
-                          labelText: 'Case Number',
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              width: _clientEmailFocused ? 3 : 1,
-                              color: Colors.pink,
-                            ),
-                            borderRadius: BorderRadius.circular(20.0),
-                          ),
-                          filled: true,
-                          fillColor: Colors.grey[900],
-                          icon: const Icon(
-                            Icons.numbers,
-                            color: Colors.pink,
-                          )),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter the client\'s email';
-                        }
-                        return null;
-                      },
-                      onTap: () {
-                        setState(() {
-                          _clientEmailFocused = true;
-                          _clientNameFocused = false;
-                          _ipcSectionsFocused = false;
-                          _nextHearingDateFocused = false;
-                        });
-                      },
-                    ),
-                    const SizedBox(height: 10),
-                    TextFormField(
-                      controller: _lawyerEmailController,
-                      decoration: InputDecoration(
-                          labelText: 'Lawyer Email',
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              width: _lawyerEmailFocused ? 3 : 1,
-                              color: Colors.green,
-                            ),
-                            borderRadius: BorderRadius.circular(20.0),
-                          ),
-                          filled: true,
-                          fillColor: Colors.grey[900],
-                          icon: const Icon(
-                            Icons.email,
-                            color: Colors.green,
-                          )),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter the lawyer\'s email';
-                        }
-                        return null;
-                      },
-                      onTap: () {
-                        setState(() {
-                          _clientEmailFocused = false;
-                          _lawyerEmailFocused = true;
-                          _clientNameFocused = false;
-                          _ipcSectionsFocused = false;
-                          _nextHearingDateFocused = false;
-                        });
-                      },
-                    ),
-                    const SizedBox(height: 10),
-                    TextFormField(
-                      controller: _clientNameController,
-                      decoration: InputDecoration(
-                        labelText: 'Undertrial Name',
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            width: _clientNameFocused ? 3 : 1,
-                            color: Colors.redAccent,
-                          ),
-                          borderRadius: BorderRadius.circular(20.0),
+              color: Colors.grey[900],
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      _buildTextFormField(
+                        controller: _clientEmailController,
+                        label: 'Undertrial Email',
+                        icon: Icons.email,
+                      ),
+                      _buildTextFormField(
+                        controller: _caseNumberController,
+                        label: 'Case Number',
+                        icon: Icons.numbers,
+                      ),
+                      _buildTextFormField(
+                        controller: _lawyerEmailController,
+                        label: 'Lawyer Email',
+                        icon: Icons.email,
+                      ),
+                      _buildTextFormField(
+                        controller: _clientNameController,
+                        label: 'Undertrial Name',
+                        icon: Icons.person,
+                      ),
+                      _buildTextFormField(
+                        controller: _ipcSectionsController,
+                        label: 'IPC Sections Violated',
+                        icon: Icons.gavel,
+                      ),
+                      _buildDateField(
+                        label: 'Next Hearing Date',
+                        controller: _nextHearingDateController,
+                        onTap: () => _selectDate(context),
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: _submitCase,
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.deepPurple, // Button color
                         ),
-                        filled: true,
-                        fillColor: Colors.grey[900],
-                        icon: const Icon(Icons.person, color: Colors.red),
+                        child: const Text('Submit'),
                       ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter the Undertrial\'s name';
-                        }
-                        return null;
-                      },
-                      onTap: () {
-                        setState(() {
-                          _clientEmailFocused = false;
-                          _clientNameFocused = true;
-                          _ipcSectionsFocused = false;
-                          _nextHearingDateFocused = false;
-                        });
-                      },
-                    ),
-                    const SizedBox(height: 10),
-                    TextFormField(
-                      controller: _ipcSectionsController,
-                      decoration: InputDecoration(
-                        labelText: 'IPC Sections Violated',
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            width: _ipcSectionsFocused ? 3 : 1,
-                            color: Colors.yellowAccent,
-                          ),
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        filled: true,
-                        fillColor: Colors.grey[900],
-                        icon:
-                            const Icon(Icons.assignment, color: Colors.yellow),
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter the IPC sections violated';
-                        }
-                        return null;
-                      },
-                      onTap: () {
-                        setState(() {
-                          _clientEmailFocused = false;
-                          _clientNameFocused = false;
-                          _ipcSectionsFocused = true;
-                          _nextHearingDateFocused = false;
-                        });
-                      },
-                    ),
-                    const SizedBox(height: 10),
-                    GestureDetector(
-                      onTap: () => _selectDate(context),
-                      child: AbsorbPointer(
-                        child: TextFormField(
-                          controller: _nextHearingDateController,
-                          decoration: InputDecoration(
-                            labelText: 'Next Hearing Date',
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                width: _nextHearingDateFocused ? 3 : 1,
-                                color: Colors.white,
-                              ),
-                              borderRadius: BorderRadius.circular(20.0),
-                            ),
-                            filled: true,
-                            fillColor: Colors.grey[900],
-                            suffixIcon: const Icon(Icons.calendar_today,
-                                color: Colors.green),
-                            icon: const Icon(Icons.date_range,
-                                color: Colors.white),
-                          ),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please select the next hearing date';
-                            }
-                            return null;
-                          },
-                          onTap: () {
-                            setState(() {
-                              _clientEmailFocused = false;
-                              _clientNameFocused = false;
-                              _ipcSectionsFocused = false;
-                              _nextHearingDateFocused = true;
-                            });
-                          },
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: _submitCase,
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.amberAccent),
-                      ),
-                      child: const Text(
-                        'Submit',
-                        style: TextStyle(color: Colors.black, fontSize: 16),
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
-            const Spacer(),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextFormField(
+      {required TextEditingController controller,
+      required String label,
+      required IconData icon}) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: TextFormField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: label,
+          prefixIcon: Icon(icon, color: Colors.deepPurple),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25.0),
+          ),
+          filled: true,
+          fillColor: Colors.grey[850],
+        ),
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter $label';
+          }
+          return null;
+        },
+      ),
+    );
+  }
+
+  Widget _buildDateField(
+      {required String label,
+      required TextEditingController controller,
+      required VoidCallback onTap}) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: GestureDetector(
+        onTap: onTap,
+        child: AbsorbPointer(
+          child: TextFormField(
+            controller: controller,
+            decoration: InputDecoration(
+              labelText: label,
+              prefixIcon:
+                  const Icon(Icons.date_range, color: Colors.deepPurple),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(25.0),
+              ),
+              filled: true,
+              fillColor: Colors.grey[850],
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please select $label';
+              }
+              return null;
+            },
+          ),
         ),
       ),
     );
